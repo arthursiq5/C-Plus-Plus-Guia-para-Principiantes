d2ecc9ae37d6a7eaf9ca091cf3131f5c927c5690
Arthur Siqueira <arthur.siq5@gmail.com>
2019-12-23 11:06:09 -0300

    programa de uso básico de variáveis

---
+++ modulo_1/VarDemo.cpp
@@ -0,0 +1,20 @@
+/*
+ * Usando uma variável
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int length; // declaração de variável
+
+	length = 7; // atribuição de valor à variável
+
+	cout << "O comprimento é ";
+	cout << length;
+	cout << "\n";
+
+	return 0;
+}
